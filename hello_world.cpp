a5554d4332ed377cb729d581a5ffe1dea6211b49
LolKIa <remeyztoor@gmail.com>
2024-10-04 01:19:12 +0900

    comments language changed

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
+
 int main() {  
-    // Запрашивает имя пользователя
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    // Выводит сообщение
-    std::cout << "Hello world from " << name << std::endl;
+    // Requests the user's first and last name
+    std::string firstName, lastName;
+    std::cout << "Enter your first name: ";
+    std::cin >> firstName;
+    std::cout << "Enter your last name: ";
+    std::cin >> lastName;
+
+    // Displays a message
+    std::cout << "Hello world from " << firstName << " " << lastName << std::endl;
     return 0;
 }
