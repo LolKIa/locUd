f8dd9c4265f58aeaad58bc2ba34537fb0c1d24f6
LolKIa <remeyztoor@gmail.com>
2024-10-04 01:04:16 +0900

    modified code

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,14 +2,22 @@
 #include <string>
 
 int main() {  
+<<<<<<< HEAD
     // Requests the user's first and last name
+=======
+    // Запрашивает имя и фамилию пользователя
+>>>>>>> 606eda9 (modified code)
     std::string firstName, lastName;
     std::cout << "Enter your first name: ";
     std::cin >> firstName;
     std::cout << "Enter your last name: ";
     std::cin >> lastName;
 
+<<<<<<< HEAD
     // Displays a message
+=======
+    // Выводит сообщение
+>>>>>>> 606eda9 (modified code)
     std::cout << "Hello world from " << firstName << " " << lastName << std::endl;
     return 0;
 }
